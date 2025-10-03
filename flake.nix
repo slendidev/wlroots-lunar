28ed6475f5d316693b815710c62e5578300608a5
Slendi <slendi@socopon.com>
2025-10-03 15:47:52 +0300

    Update
    Signed-off-by: Slendi <slendi@socopon.com>

--- flake.nix
+++ flake.nix
@@ -3,79 +3,88 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-      wlrootsLocal = pkgs.stdenv.mkDerivation {
-        pname = "wlroots";
-        version = "local";
-
-        src = ./.;
-
-        nativeBuildInputs = with pkgs; [
-          meson
-          ninja
-          pkg-config
-          wayland-scanner
-          glslang
-          hwdata
-        ];
-
-        buildInputs = with pkgs; [
-          libliftoff
-          libdisplay-info
-          libGL
-          libcap
-          libinput
-          libxkbcommon
-          libgbm
-          pixman
-          seatd
-          vulkan-loader
-          wayland
-          wayland-protocols
-          xorg.libX11
-          xorg.xcbutilerrors
-          xorg.xcbutilimage
-          xorg.xcbutilrenderutil
-          xorg.xcbutilwm
-          lcms2
-        ];
-
-        mesonFlags = [
-          "-Dxwayland=disabled"
-        ];
-
-        # Optionally, your postFixup from above:
-        postFixup = ''
-          					mkdir -p $examples/bin
-          					cd ./examples
-          					for binary in $(find . -executable -type f -printf '%P\n' | grep -vE '\.so'); do
-          						cp "$binary" "$examples/bin/wlroots-$binary"
-          					done
-          				'';
-
-        outputs = [
-          "out"
-          "examples"
-        ];
-
-        meta = with pkgs.lib; {
-          description = "Modular Wayland compositor library (local build)";
-          license = licenses.mit;
-          platforms = platforms.linux;
-        };
-      };
-    in
     {
-      packages.${system}.default = wlrootsLocal;
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        wlrootsLocal = pkgs.stdenv.mkDerivation {
+          pname = "wlroots";
+          version = "local";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            meson
+            ninja
+            pkg-config
+            wayland-scanner
+            glslang
+            hwdata
+            wayland
+          ];
+
+          buildInputs = with pkgs; [
+            libliftoff
+            libdisplay-info
+            libGL
+            libcap
+            libinput
+            libxkbcommon
+            libgbm
+            pixman
+            seatd
+            vulkan-loader
+            wayland
+            wayland-protocols
+            xorg.libX11
+            xorg.xcbutilerrors
+            xorg.xcbutilimage
+            xorg.xcbutilrenderutil
+            xorg.xcbutilwm
+            lcms2
+            cmake
+          ];
+
+          mesonFlags = [
+            "-Dxwayland=disabled"
+          ];
+
+          # Optionally, your postFixup from above:
+          postFixup = ''
+            					mkdir -p $examples/bin
+            					cd ./examples
+            					for binary in $(find . -executable -type f -printf '%P\n' | grep -vE '\.so'); do
+            						cp "$binary" "$examples/bin/wlroots-$binary"
+            					done
+            				'';
+
+          outputs = [
+            "out"
+            "examples"
+          ];
+
+          meta = with pkgs.lib; {
+            description = "Modular Wayland compositor library (local build)";
+            license = licenses.mit;
+            platforms = platforms.linux;
+          };
+        };
+      in
+      {
+        packages.default = wlrootsLocal;
+      }
+    );
 }
